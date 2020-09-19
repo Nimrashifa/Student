f266cf3bf2cc7de6fa2fda86a6873fc1d4bb830f
nimra <nimra@DESKTOP-1TV34D9.myfiosgateway.com>
2020-09-18 22:02:46 -0400

    week2 Assignment Student class

---
+++ StudentInfo/src/studentInfo/Student.java
@@ -0,0 +1,15 @@
+package studentInfo;
+
+public class Student {
+   int studentAge;
+   String studentName;
+   public Student(String studentName) {
+	   this.studentName = studentName;
+   }
+   public Student(int studentage ) {
+	   
+	   this.studentAge = studentage;
+	   
+   }
+   
+}
---
+++ StudentInfo/src/studentInfo/StudentInfo.java
@@ -0,0 +1,22 @@
+package studentInfo;
+
+public class StudentInfo {
+	public static void main(String[] args){
+		Student s = new Student("Sania");
+		Student s1= new Student(20);
+		
+		System.out.println(s.studentName + " is " + s1.studentAge);
+
+	    		
+	 
+	    
+	    
+		
+		
+		
+				
+				
+		
+	}
+
+}
